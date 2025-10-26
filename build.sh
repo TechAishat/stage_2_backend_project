d04a44f2ab13c44424023539b941929a6db3458f
Aishat <aaishat122@gmail.com>
2025-10-26 22:37:53 +0100

    Update build script with essential commands

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 # Exit on error
 set -o errexit
 
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
 
 # Apply database migrations
 python manage.py migrate
